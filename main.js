758c333a8b1473b9908cfcf3b2a10c0cbdc440a3
AustinEgger <55104586+AustinEgger@users.noreply.github.com>
2022-10-20 15:50:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+/*
+uses js bootstrap
 $(document).ready(function(){
 	$('.header').height($(window).height());
+})*/
+/*plan js for sstretching eement across screen*/
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
 })
